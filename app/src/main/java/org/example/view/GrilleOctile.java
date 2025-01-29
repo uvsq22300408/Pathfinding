0669b0e1ec377d2d2167454b2bb528320f8f53f1
Théotime <=>
2025-01-29 02:35:10 +0100

    Ajout d'une dépendence pour Raylib et d'un fichier simple l'utilisant

---
+++ app/src/main/java/org/example/view/GrilleOctile.java
@@ -0,0 +1,13 @@
+package org.example.view;
+import static com.raylib.Raylib.*;
+public class GrilleOctile {
+    
+    public static void main(String args[]) {
+        InitWindow(800, 600, "Pathfinding");
+        SetTargetFPS(60);
+        while(!WindowShouldClose()) {
+            
+        }
+        CloseWindow();
+    }
+}
