83b3a6a68783c2839732b13e71437cb69ef65f11
Théotime <=>
2025-02-04 23:43:07 +0100

    Finition classe world

--- app/src/main/java/org/example/world/World.java
+++ app/src/main/java/org/example/world/World.java
@@ -1,5 +1,6 @@
 package org.example.world;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class World {
@@ -12,23 +13,26 @@ public class World {
         tailleReg = _tailleRegion;
         start = _start;
         destination = _destination;
+        info = new InfoWorld();
         Region startReg, destinationRegion;
         switch(regtype) {
             case OCTILE: {
                 startReg = new Region.Octile(start.x, start.y, tailleReg);
                 destinationRegion = new Region.Octile(destination.x, destination.y, tailleReg);
+                break;
             }
             default: {
                 startReg = new Region.Octile(start.x, start.y, tailleReg);
                 destinationRegion = new Region.Octile(destination.x, destination.y, tailleReg);
             }
         }
-        passThrough = new int[(w / _tailleRegion) * (h / _tailleRegion)];
-        for (int ix = 0; ix < (w / _tailleRegion) * (h / _tailleRegion); ix++) {
+        passThrough = new int[info.widthInRegion * info.heightInRegion];
+        for (int ix = 0; ix < info.widthInRegion * info.heightInRegion; ix++) {
             passThrough[ix] = 0;
         }
-        passThrough[(startReg.x / tailleReg) * (h / tailleReg) + (startReg.y / tailleReg)] = InnerWorld.START;
-        passThrough[(destinationRegion.x / tailleReg) * (h / tailleReg) + (destinationRegion.y / tailleReg)] = InnerWorld.DESTINATION;
+        passThrough[(startReg.x / tailleReg) * info.heightInRegion + (startReg.y / tailleReg)] = InnerWorld.START;
+        passThrough[(destinationRegion.x / tailleReg) *  info.heightInRegion + (destinationRegion.y / tailleReg)] = InnerWorld.DESTINATION;
+        
     }
 
     public enum ERegionType {
@@ -43,7 +47,15 @@ public class World {
             int regionStartY = Math.round(y) / tailleReg;
             this.x = regionStartX * tailleReg;
             this.y = regionStartY * tailleReg;
+            this.distance = 0;
         }
+
+        public Region(int x, int y, int tailleReg, double dist) {
+            this.x = x;
+            this.y = y;
+            this.distance = dist;
+        }
+
         public static class Octile extends Region {
             public Octile(float x, float y, int tailleReg) {
                 super(x, y, tailleReg);
@@ -56,8 +68,9 @@ public class World {
             }
         }
 
-        public int x;
-        public int y;
+        public final int x;
+        public final int y;
+        public final double distance;
     }
 
     public static class Obstacle {
@@ -92,6 +105,47 @@ public class World {
         public static int START = -80;
         public static int DESTINATION = -90;
     }
+
+    public class InfoWorld {
+        public InfoWorld() {
+
+            switch (regiontype) {
+                case OCTILE:
+                    widthInRegion = width / tailleReg;
+                    heightInRegion = height / tailleReg;
+                    nbRegion = widthInRegion * heightInRegion;
+                    break;
+                default:
+                    widthInRegion = 0;
+                    heightInRegion = 0;
+            }
+        }
+
+        public List<Region> getAdjacent(Region r) {
+            List<Region> neighborhood = new ArrayList<>();
+            switch (regiontype) {
+                case OCTILE:
+                    // We go through each adjacent region
+                    // and store those that are not obstacles
+                    for (int x = r.x - 1; x < r.x + 2; x++) {
+                        if (x <= 0 || x >= widthInRegion) continue;
+                        for (int y = r.y - 1; y < r.y - 2; y++) {
+                            if (y <= 0 || y >= heightInRegion) continue;
+                            Region neigh = new Region(x, y, tailleReg, Math.sqrt(x + y));
+                            neighborhood.add(neigh);
+                        }
+                    }
+                    break;
+                default:
+                    return null;
+            }
+            return neighborhood;
+        }
+
+        public int widthInRegion;
+        public int heightInRegion;
+        public int nbRegion;
+    }
     
 
     public int tailleReg;
@@ -103,5 +157,6 @@ public class World {
     public Point destination;
     public Region startReg;
     public Region destinationReg;
-    public int[] passThrough; 
+    public int[] passThrough;
+    public InfoWorld info; 
 }
